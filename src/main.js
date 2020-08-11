cf45e841acfc3dfeadc1c7a99534ac2eb34e67e5
WakerCN <1209562577@qq.com>
2020-08-11 12:28:29 +0800

    基本页面组件拆分

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/* 全局组件 begin */
+import Header from './components/Header'
+import TabBar from './components/TabBar'
+
+Vue.component('Header', Header)
+Vue.component('TabBar', TabBar)
+/* 全局组件 end */
+
 Vue.config.productionTip = false
 
 new Vue({
